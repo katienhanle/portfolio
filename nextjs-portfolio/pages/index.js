78886a24db4e164774f442a10d39657a709ee788
kal070 <kal070@ucsd.edu>
2025-10-21 10:28:59 -0700

    New Resume

--- nextjs-portfolio/pages/index.js
+++ nextjs-portfolio/pages/index.js
@@ -644,7 +644,7 @@ export default function Home() {
             <h3 style={{ margin: 0, fontSize: '1.4rem', fontWeight: 800 }}>Katie Nhan Le</h3>
             <div style={{ height: 10 }} />
             <a
-              href="/KatieNhanLeResume_Creative.pdf"
+              href="/KatieNhanLe_Resume_Creative.pdf"
               target="_blank"
               rel="noopener noreferrer"
               style={{
